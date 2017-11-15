4c732d01b6674c4e0b0b686bc53a84f291de5740
LXX <13895167943@163.com>
2017-11-15 16:01:36 +0800

    add:springboot中注册servlet，filter，listener的方法。网址：www.vxzsk.com/514.html

--- src/main/java/com/websocket/demo/WebSocketApplication.java
+++ src/main/java/com/websocket/demo/WebSocketApplication.java
@@ -1,12 +1,48 @@
 package com.websocket.demo;
 
+import com.websocket.demo.servlet.Myservlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
+@ServletComponentScan
+/**
+ * 在 SpringBootApplication上使用@ServletComponentScan 注解后，
+ * Servlet、Filter、Listener 可以直接通过 @WebServlet、@WebFilter、@WebListener 注解自动注册，无需其他代码
+ */
 public class WebSocketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebSocketApplication.class, args);
+	}
+
+
+	/**
+	 * 使用代码注册Servlet不需要使用@ServletComponentScan注解
+	 * 代码注册通过ServletRegistrationBean、 FilterRegistrationBean 和 ServletListenerRegistrationBean 获得控制。
+	 * 也可以通过实现 ServletContextInitializer 接口直接注册。
+	 * @return
+	 */
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean(){
+		return new ServletRegistrationBean(new Myservlet(),"/xs/*");
+	}
+
+	/**
+	 * 修改DispatcherServlet默认配置
+	 * @param dispatcherServlet
+	 * @return
+	 */
+//	@Bean
+	public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet){
+		ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet);
+		registration.getUrlMappings().clear();
+		registration.addUrlMappings("*.do");
+		registration.addUrlMappings("*.jsp");
+		return registration;
 	}
 }
---
+++ src/main/java/com/websocket/demo/servlet/MyServlet2.java
@@ -0,0 +1,13 @@
+package com.websocket.demo.servlet;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.Set;
+
+public class MyServlet2 implements ServletContainerInitializer{
+    @Override
+    public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
+
+    }
+}
