b53d64be958f5b1820978e0ed4435bfa8d4eb845
LXX <13895167943@163.com>
2017-11-15 16:39:52 +0800

    add:springboot 中使用拦截器Intercepter。 1、创建我们自己的拦截器类并实现 HandlerInterceptor 接口。 2、创建一个Java类继承WebMvcConfigurerAdapter，并重写 addInterceptors 方法。 2、实例化我们自定义的拦截器，然后将对像手动添加到拦截器链中（在addInterceptors方法中添加） 网址：http://www.vxzsk.com/559.html

--- src/main/java/com/websocket/demo/config/WebMvcConfig.java
+++ src/main/java/com/websocket/demo/config/WebMvcConfig.java
@@ -1,6 +1,9 @@
 package com.websocket.demo.config;
 
+import com.websocket.demo.intercepteor.MyInterceptor1;
+import com.websocket.demo.intercepteor.MyInterceptor2;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -20,5 +23,19 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter{
         registry.addViewController("/ws").setViewName("/ws");
         registry.addViewController("/ws2").setViewName("/ws2");
         registry.addViewController("/websocket/index.html").setViewName("/websocket/index.html");
+    }
+
+    /**
+     * 添加自定义拦截器
+     * 多个拦截器组成一个拦截器链
+     * addPathPatterns 用于添加拦截规则
+     * excludePathPatterns 用户排除拦截
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+//        super.addInterceptors(registry);
+        registry.addInterceptor(new MyInterceptor1()).addPathPatterns("/**");
+        registry.addInterceptor(new MyInterceptor2()).addPathPatterns("/**");
     }
 }
