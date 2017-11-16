874fc6d72ce558c6f53bab9749b0cdba6fd615a5
LXX <13895167943@163.com>
2017-11-16 10:16:52 +0800

    add:spring spring boot banner的设置 网址：http://www.vxzsk.com/625.html

--- src/main/java/com/websocket/demo/WebSocketApplication.java
+++ src/main/java/com/websocket/demo/WebSocketApplication.java
@@ -1,6 +1,7 @@
 package com.websocket.demo;
 
 import com.websocket.demo.servlet.Myservlet;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -17,7 +18,15 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class WebSocketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebSocketApplication.class, args);
+//		SpringApplication.run(WebSocketApplication.class, args);
+		SpringApplication application = new SpringApplication(WebSocketApplication.class);
+		/*
+         * Banner.Mode.OFF:关闭;
+         * Banner.Mode.CONSOLE:控制台输出，默认方式;
+         * Banner.Mode.LOG:日志输出方式;
+         */
+		application.setBannerMode(Banner.Mode.OFF);
+		application.run(args);
 	}
 
 
